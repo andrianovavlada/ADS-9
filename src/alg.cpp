865fbddb97463e340b27fabcb1a252122b051083
Andrianova Vlada <99177026+andrianovavlada@users.noreply.github.com>
2022-05-26 16:13:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,12 +8,12 @@
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
   BST<std::string> Tree;
+  std::string word = "";
 std::ifstream file(filename);
 if (!file) {
     std::cout << "File error!" << std::endl;
     return Tree;
 }
-std::string word = "";
 while (!file.eof()) {
     char sign = file.get();
     if (sign <= 'Z' && sign >= 'A') {
